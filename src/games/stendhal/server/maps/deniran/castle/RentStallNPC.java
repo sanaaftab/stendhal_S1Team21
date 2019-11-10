e648be12d451c339f8fc4b802f99e986416bafd9
Sana Aftab <sana.aftab-2@student.manchester.ac.uk>
2019-11-10 15:31:26 +0000

    Test whether the NPC is in the required zone and its basic conversation
    When the player talks to the NPC in the Deniran Castle, the NPC will
    respond to greetings and 'job'.

---
+++ src/games/stendhal/server/maps/deniran/castle/RentStallNPC.java
@@ -0,0 +1,16 @@
+package games.stendhal.server.maps.deniran.castle;
+
+import java.util.Map;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+
+public class RentStallNPC implements ZoneConfigurator {
+
+	@Override
+	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
