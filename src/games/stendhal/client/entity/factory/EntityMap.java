a2bbe7ed2199e37881f973751931d123a7a3c805
Shaurya Gautam <shaurya.gautam@student.manchester.ac.uk>
2019-11-14 11:28:22 +0000

    Registered Newspaper class
    Registered the newspaper item in the game.

--- src/games/stendhal/client/entity/factory/EntityMap.java
+++ src/games/stendhal/client/entity/factory/EntityMap.java
@@ -166,6 +166,7 @@ public final class EntityMap {
 		register("weather_entity", null, null, InvisibleEntity.class);
 
 		register("looped_sound_source", null, null, LoopedSoundSource.class);
+		register("newspaper", null, null, Item.class);
 	}
 
 	/**
